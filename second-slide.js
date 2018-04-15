e24a18f6a2dec00fc23d39900a526b9d9217f788
hamzamoha <38383309+hamzamoha@users.noreply.github.com>
2018-04-15 12:26:00 +0100

    Create second-slide.js

---
+++ second-slide.js
@@ -0,0 +1,10 @@
+var maxpost=3;var i=1;var y=1;
+document.getElementById('slide-style').innerHTML = '#slide #post:nth-of-type(' + i + '){z-index:1;opacity:1;}';
+function next(){
+if (i==maxpost){i=1;document.getElementById('slide-style').innerHTML = '#slide #post{z-index:0;opacity:0;}#slide #post:nth-of-type(' + i + '){z-index:1;opacity:1;}';}else {i=i+1;document.getElementById('slide-style').innerHTML = '#slide #post{z-index:0;opacity:0;}#slide #post:nth-of-type(' + i + '){z-index:1;opacity:1;}';}}
+function previous(){
+if (i==1){i=maxpost;document.getElementById('slide-style').innerHTML = '#slide #post{z-index:0;opacity:0;}#slide #post:nth-of-type(' + i + '){z-index:1;opacity:1;}';}else {i=i-1;document.getElementById('slide-style').innerHTML = '#slide #post{z-index:0;opacity:0;}#slide #post:nth-of-type(' + i + '){z-index:1;opacity:1;}';}}
+function stop(){y=0;}
+function start(){y=1;setTimeout(refreshData, 3000);}
+function refreshData(){if(y){x=3;next();setTimeout(refreshData, x*1000);}}
+setTimeout(refreshData, 3000);
