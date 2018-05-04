1af7c9bdc87a9e516ce37a800818f7fcbfbce29d
hamzamoha <38383309+hamzamoha@users.noreply.github.com>
2018-05-05 00:10:25 +0100

    Update second-slide.js

--- second-slide.js
+++ second-slide.js
@@ -1,10 +1,16 @@
-var i=1;var y=1;
-document.getElementById('slide-style').innerHTML = '#slide #post:nth-of-type(' + i + '){z-index:1;opacity:1;}';
-function next(){
-if (i==maxres){i=1;document.getElementById('slide-style').innerHTML = '#slide #post{z-index:0;opacity:0;}#slide #post:nth-of-type(' + i + '){z-index:1;opacity:1;}';}else {i=i+1;document.getElementById('slide-style').innerHTML = '#slide #post{z-index:0;opacity:0;}#slide #post:nth-of-type(' + i + '){z-index:1;opacity:1;}';}}
-function previous(){
-if (i==1){i=maxres;document.getElementById('slide-style').innerHTML = '#slide #post{z-index:0;opacity:0;}#slide #post:nth-of-type(' + i + '){z-index:1;opacity:1;}';}else {i=i-1;document.getElementById('slide-style').innerHTML = '#slide #post{z-index:0;opacity:0;}#slide #post:nth-of-type(' + i + '){z-index:1;opacity:1;}';}}
-function stop(){y=0;}
-function start(){y=1;setTimeout(refreshData, 3000);}
-function refreshData(){if(y){x=3;next();setTimeout(refreshData, x*1000);}}
-setTimeout(refreshData, 3000);
+var gens = document.getElementById('genius-slide');
+var xwidth = document.documentElement.clientWidth;
+var l = xwidth;
+var loopp = 1;
+gens.style.width = maxres*xwidth+'px';
+for(i=0;i<gens.children.length;i++){
+if (gens.children[i].id == 'post'){
+gens.children[i].style.width = xwidth+'px';
+}}
+function myfun(){
+gens.style.transform = 'translatex('+l+'px)';
+if (l==xwidth*maxres-xwidth){l=0}
+else {l=l+xwidth}
+if(loopp){setTimeout(myfun, 6000)};
+}
+setTimeout(myfun, 6000);
