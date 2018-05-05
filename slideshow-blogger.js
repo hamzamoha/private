69e73e53ec644ecad00a0e0ac7180285180666c1
hamzamoha <38383309+hamzamoha@users.noreply.github.com>
2018-05-05 20:30:15 +0100

    Update slideshow-blogger.js

--- slideshow-blogger.js
+++ slideshow-blogger.js
@@ -1,16 +1,27 @@
 function mycallback(json) {
-    for (var i = 0; i < json.feed.entry.length; i++) {
-      for (var j = 0; j < json.feed.entry[i].link.length; j++) {
-        if (json.feed.entry[i].link[j].rel == 'alternate') {
-          var postUrl = json.feed.entry[i].link[j].href;
+    for (var i=0,x=json.feed.entry; i < x.length; i++) {
+      for (var j = 0; j < x[i].link.length; j++) {
+        if (x[i].link[j].rel == 'alternate') {
+          var postUrl = x[i].link[j].href;
           break;
         }
       }
-	  //var sthumbnailurl = json.feed.entry[i].media$thumbnail.url;
-	  //var thumb = sthumbnailurl.replace('s72-c','s1600');
-      var postTitle = json.feed.entry[i].title.$t;
-      //var postSummary = json.feed.entry[i].summary.$t;
-      var item ='<div id="post"><div id="picture"><img src=' + /*thumb*/'' + '><div id="cover"></div></div><div id="title">' + postTitle + '</div><div id="read-more"><a href=' + postUrl + '>Read more</a></div></div>';
+    if(x[i].media$thumbnail.url==(null || undefined)){
+    	var content = x[i].content.$t;
+	var imgtag = content.indexOf('<img');
+	var srcatr = content.indexOf(' src=',imgtag);
+	var start = srcatr+6;
+	var end = content.indexOf('"',start);
+	var src = content.substring(start,end);
+	var thmb = src;
+    }
+    else {
+        var sthumbnailurl = x[i].media$thumbnail.url;
+	var thumb = sthumbnailurl.replace('s72-c','s1600');
+    }
+      var postTitle = x[i].title.$t;
+      //var postSummary = x[i].summary.$t;
+      var item ='<div id="post"><div id="picture"><img src=' + thumb + '><div id="cover"></div></div><div id="title">' + postTitle + '</div><div id="read-more"><a href=' + postUrl + '>Read more</a></div></div>';
       document.write(item);
     }
   }
