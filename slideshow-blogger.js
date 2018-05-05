77479aeff03604558fd1e0fe25d23b8e96048e3f
hamzamoha <38383309+hamzamoha@users.noreply.github.com>
2018-05-05 19:55:51 +0100

    Update slideshow-blogger.js

--- slideshow-blogger.js
+++ slideshow-blogger.js
@@ -10,8 +10,8 @@ function mycallback(json) {
 	  var thumb = sthumbnailurl.replace('s72-c','s1600');
       var postTitle = json.feed.entry[i].title.$t;
       //var postSummary = json.feed.entry[i].summary.$t;
-      var item ='<div id="post"><div id="picture"><img src=' + thumb + '><div id="cover"></div></div><div id="title">' + postTitle + '</div><div id="read-more"><a href=' + postUrl + '>Read more</a></div></div>';
+      var item ='<div id="post"><div id="picture"><img src=' + /*thumb*/'' + '><div id="cover"></div></div><div id="title">' + postTitle + '</div><div id="read-more"><a href=' + postUrl + '>Read more</a></div></div>';
       document.write(item);
     }
   }
-document.write('<scr'+'ipt type="text/javascript" src="https://'+blogurl+'/feeds/posts/summary?max-results='+maxres+'&alt=json-in-script&callback=mycallback" ></scr'+'ipt>');
+document.write('<scr'+'ipt type="text/javascript" src="https://'+blogurl+'/feeds/posts/default?max-results='+maxres+'&alt=json-in-script&callback=mycallback" ></scr'+'ipt>');
