b53e8cdefd4e98c4449c0b35752eb2620f465f93
hamzamoha <38383309+hamzamoha@users.noreply.github.com>
2018-06-09 08:14:01 +0000

    Delete big-thumbnail-for-blogger.js

--- big-thumbnail-for-blogger.js
+++
@@ -1,8 +0,0 @@
-var i = document.getElementsByTagName('img');
-var arr=[];
-  for(var j=0;j<i.length;j++){
-    if(i[j].parentElement.id=='thumbnail'){arr.push(i[j])}
-}
-  for(var j=0;j<arr.length;j++){
-	arr[j].src=arr[j].src.replace('s72-c', 's1600');
-}
